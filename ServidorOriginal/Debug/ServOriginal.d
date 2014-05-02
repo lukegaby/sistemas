9eab7da016dcfb340b9ab8ec205ab83b43b8f319
lukegaby <lukegaby77@gmail.com>
2014-05-02 20:36:38 -0300

    elimino todo

--- ServidorOriginal/Debug/ServOriginal.d
+++
@@ -1,9 +0,0 @@
-ServOriginal.d: ../ServOriginal.c ../ServOriginal.h \
- /home/lucas/workspace/commons/commons/string.c \
- /home/lucas/workspace/commons/commons/string.h
-
-../ServOriginal.h:
-
-/home/lucas/workspace/commons/commons/string.c:
-
-/home/lucas/workspace/commons/commons/string.h:
