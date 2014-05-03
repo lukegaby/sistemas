6de0f374cc03b39440ac21da0fbd3e716cd2dd47
lukegaby <lukegaby77@gmail.com>
2014-05-02 21:06:07 -0300

    subo todo de nuevo

---
+++ cpu/Debug/src/cpu.d
@@ -0,0 +1,26 @@
+src/cpu.d: ../src/cpu.c /home/lucas/git/sistemas/commons/commons/config.h \
+ /home/lucas/git/sistemas/commons/commons/collections/dictionary.h \
+ /home/lucas/git/sistemas/commons/commons/collections/node.h \
+ /home/lucas/git/sistemas/commons/commons/string.h \
+ /home/lucas/git/sistemas/parser/parser/parser.h \
+ /home/lucas/git/sistemas/parser/parser/sintax.h \
+ /home/lucas/git/sistemas/commons/commons/collections/dictionary.h \
+ /home/lucas/git/sistemas/commons/commons/error.h ../src/cpu.h
+
+/home/lucas/git/sistemas/commons/commons/config.h:
+
+/home/lucas/git/sistemas/commons/commons/collections/dictionary.h:
+
+/home/lucas/git/sistemas/commons/commons/collections/node.h:
+
+/home/lucas/git/sistemas/commons/commons/string.h:
+
+/home/lucas/git/sistemas/parser/parser/parser.h:
+
+/home/lucas/git/sistemas/parser/parser/sintax.h:
+
+/home/lucas/git/sistemas/commons/commons/collections/dictionary.h:
+
+/home/lucas/git/sistemas/commons/commons/error.h:
+
+../src/cpu.h:
